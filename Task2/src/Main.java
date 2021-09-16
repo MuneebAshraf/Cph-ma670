097d9ca84de8c9ce3ada590c6746dcc37589188a
MuneebAshraf <Munne_122@hotmail.com>
2021-09-16 21:22:56 +0200

    Tilføjet forskelliige tasks og ovelser fra bogen

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
-    public static class person {
-        String name = "Muneeb";
-        int age = 26;
-        boolean happy = false;
-    }
-
     public static void main(String[]args){
-        person mee = new person();
 
-        System.out.printf("Hi, my name is %s\n",mee.name);
-        System.out.printf("I am %s years old%n",mee.age);
+        person mee = new person( "Muneeb", 26, false);
+
+        System.out.printf("Hi, my name is %s \n",mee.name);
+        System.out.printf("I am %s years old",mee.age);
         if (mee.happy) {
             System.out.println("I clap my hands");
         } else{
