fc51ca1fa841edd93a3c8d0bd8661429863e1e49
MuneebAshraf <Munne_122@hotmail.com>
2021-09-30 12:33:42 +0200

    ovelse for 30.09.21
    all tasks

--- Ovelser 2 23.09.21/TaskSix/src/Oops.java
+++ Ovelser 2 23.09.21/TaskSix/src/Oops.java
@@ -10,13 +10,14 @@
 public class Oops {
     public static void main(String[] args) {
         int a = 7, b = 42;
-        int smaller = minimum(a,b);         //added smaller= before minimum
+        int smaller = minimum(a, b);         //added smaller= before minimum
         if (smaller == a) {                 //fixed from {smaller == a} to (smaller == a)
-            System.out.println(a+" is the smallest!"); // moved a outside string
+            System.out.println(a + " is the smallest!"); // moved a outside string
         }
     }
+
     public static int minimum(int a, int b) {       //changed return type from void to it
-        int smaller =0;                             //added local referance smaller
+        int smaller = 0;                             //added local referance smaller
         if (a < b) {
             smaller = a;                            //removed int smaller = a
         } else if (a >= b) {
